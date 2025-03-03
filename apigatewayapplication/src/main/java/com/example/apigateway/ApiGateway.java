5aea40c32605607ac9c2df403441f29299dec835
Anurag Bharti <anuragvasubharti@gmail.com>
2025-03-03 13:39:59 +0530

    Updated Code

--- apigatewayapplication/src/main/java/com/example/apigateway/ApiGateway.java
+++ apigatewayapplication/src/main/java/com/example/apigateway/ApiGateway.java
@@ -1,11 +1,11 @@
-package com.example.apigatewayapplication;
+package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class ApiGateway {
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(ApiGateway.class, args);
 	}
 }
