03be33528bc47ca80b6a38bcc82b27b2f8ba0a93
Anurag Bharti <anuragvasubharti@gmail.com>
2025-03-08 01:57:21 +0530

    Added logging module

---
+++ logging-service/src/main/java/com/example/logging/LoggingServiceApplication.java
@@ -0,0 +1,20 @@
+package com.example.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class LoggingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingServiceApplication.class, args);
+	}
+
+	@PostMapping("/log")
+	public String logMessage(@RequestBody String message) {
+		System.out.println("Log: " + message);
+		return "Message logged";
+	}
+}
