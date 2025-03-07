f9e28f5d352d14c06383187be257749c282d94c5
Anurag Bharti <anuragvasubharti@gmail.com>
2025-03-08 01:22:42 +0530

    Added new module

---
+++ security-service/src/main/java/com/example/securityservice/SecurityServiceApplication.java
@@ -0,0 +1,43 @@
+package com.example.securityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityServiceApplication.class, args);
+	}
+
+	@GetMapping("/login")
+	public String login(@RequestBody LoginRequest request) {
+		return "Welcome, " + request.getUsername();
+	}
+}
+
+class LoginRequest {
+
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
